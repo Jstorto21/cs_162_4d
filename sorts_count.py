b81a6d8a80261aa6b0fc3802582b843e58b6b059
Jstorto21 <stortoj@oregonstate.edu>
2023-07-17 01:10:32 -0400

    Attempt 5 function, relocation

--- sorts_count.py
+++ sorts_count.py
@@ -5,9 +5,8 @@
 
 
 def bubble_count(a_list):
-    """bubble count function"""
-    comparisons = 0  # setting variable = 0
-    exchanges = 0  # setting variable = 0
+    comparisons = 0
+    exchanges = 0
 
     for pass_num in range(len(a_list) - 1):
         for index in range(len(a_list) - 1 - pass_num):
@@ -15,24 +14,23 @@ def bubble_count(a_list):
             if a_list[index] > a_list[index + 1]:
                 temp = a_list[index]
                 a_list[index] = a_list[index + 1]
-                exchanges += 1
                 a_list[index + 1] = temp
+                exchanges += 1
     return print(comparisons, exchanges)
 
 
-def insertion_count(a_list):
-    """insertion count function"""
-    comparisons = 0  # setting variable = 0
-    exchanges = 0  # setting variable = 0
+def insertion_sort(a_list):
+    comparisons = 0
+    exchanges = 0
 
     for index in range(1, len(a_list)):
         value = a_list[index]
         pos = index - 1
+        comparisons += 1
         while pos >= 0 and a_list[pos] > value:
-            comparisons += 1
             a_list[pos + 1] = a_list[pos]
-            pos -= 1
             exchanges += 1
+            pos -= 1
+            comparisons += 1
         a_list[pos + 1] = value
     return print(comparisons, exchanges)
-
